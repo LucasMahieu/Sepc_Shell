da65a845a9bfbc6b3c5936c2a6eb00ddc21b317c
lucas <lucas.mahieu@icloud.com>
2015-10-25 19:50:59 +0100

    Ajoute de 2 fonctions pour simplifier la fonction executer, en vu de la question sur le scheme

--- src/ensishell.c
+++ src/ensishell.c
@@ -109,28 +109,112 @@ void free_jobs()
     }
 }
 
+
+int exec_simple_cmd(struct cmdline *cmd,char **cpyLine) {
+pid_t pid;
+int status;
+    switch(pid = fork()) {
+            case 0:
+                // Le fils execute ce code
+                if ((execvp(cmd->seq[0][0],cmd->seq[0])) == -1)
+                {
+                    return -1;
+                }
+                break;
+                // En cas d'erreur, on retourne -1.
+            case -1:
+                perror("error when fork creation");
+                return -1;
+                break;
+            default:
+                // Le père execute ce code
+                // Si & a été écrit, le shell s'affiche directement
+                if (cmd->bg) {
+                    add_jobs(pid, *cpyLine);
+                } else{
+                    waitpid(pid, &status, 0);
+                }
+                break;
+        }
+        free(*cpyLine);
+        return 0;
+    } 
+
+int exec_pipe_cmd(struct cmdline *cmd) {
+    pid_t pid1 , pid2;
+    int status1, status2;
+    int pid_wait;
+    int pipefd[2];
+
+        if(pipe(pipefd)){
+            perror("error in the pipe creation");
+            return -1;
+        }
+        // Creation du FIlS1 pour faire la partie droite du pipe
+        switch( pid1=fork() ) {
+            case -1:
+                //Case d'erreur du fork
+                return -1;
+                break;
+            case 0:
+                // Le fils execute ce code
+                //On dit au fils qu'il va devoir lire son entrée par le pipe et plus sur le STDIN
+                dup2(pipefd[0],0);
+                //On ferme le descriteur de fichier en ecriture et en lecture 
+                if (close(pipefd[0])) return -1;
+                if (close(pipefd[1])) return -1;
+                // Et on exec la partie gauche du pipe
+                if ((execvp(cmd->seq[1][0],cmd->seq[1])) == -1){
+                    // Cas d'erreur de l'exec: retourne -1
+                    perror("error in the exec of the children 1");
+                    return -1;
+                }
+                break;
+            default:
+                // Le père execute ce code
+                // Creation du second processus pour la partie gauche du pipe
+                if((pid2=fork()) == 0){
+                    //Le Fils 2 exec ce code
+                    // On dit au Fils2 qu'il ne va plus ecrir sur le STDOUT mais dans le pipe
+                    dup2(pipefd[1],1);
+                    //On ferme les acces au pipe 
+                    if (close(pipefd[1])) return -1;
+                    if (close(pipefd[0])) return -1;
+                    //Et on execute la partie droite du pipe
+                    if ((execvp(cmd->seq[0][0],cmd->seq[0])) == -1){
+                        // Cas d'erreur de l'exec: retourne -1
+                        perror("error in the exec of the children 2");
+                        return -1;
+                    }
+                } else if(pid2 == -1){
+                        perror("error in the fork of the children 2");
+                        return -1;
+                }
+                else{
+                    //Le Pere exec ceci 
+                }
+                pid_wait = waitpid(pid2,&status2,0);
+                printf("pere: %d ---  pid1 = %d --- pid2 = %d \n",getpid(), pid1, pid2);
+                printf("PID du proc qui se termine = %d avec status = %d \n",pid_wait, status2);
+                pid_wait = waitpid(pid1,&status1,WNOWAIT);
+                printf("PID du proc qui se termine = %d avec status = %d \n",pid_wait,status1);
+                break;
+        }
+        return 0;
+}
+
 int executer(char *line)
 {
     int i = 0, j = 0;
-    int status1;
-    pid_t pid1;
     char * cpyLine = malloc(sizeof(char) * (strlen(line) + 1));
     strcpy(cpyLine, line);
     struct cmdline *cmd = 0;
-/*
- *Variable pour le pipe
- */
-    int status2;
-    int pid_wait;
-    pid_t pid2;
-    int pipefd[2];
 
     // Parse cmd and free line
     cmd = parsecmd(&line);
 
     // If input stream is closed
-    if (!cmd)
-    {
+    if (!cmd) {
         terminate(0);
     }
 
@@ -154,101 +238,22 @@ int executer(char *line)
         printf("\n");
     }
 
-    if (!strcmp(cmd->seq[0][0],"jobs"))
-    {
+    if (!strcmp(cmd->seq[0][0],"jobs")) {
         print_jobs();
         return 0;
     }
 // Cas sans pipe 
-    if (cmd->seq[1]==0){
-        switch(pid1 = fork())
-        {
-            case 0:
-                // Le fils execute ce code
-                free(cpyLine);
-                if ((execvp(cmd->seq[0][0],cmd->seq[0])) == -1)
-                {
-                    return -1;
-                }
-                break;
-                // En cas d'erreur, on retourne -1.
-            case -1:
-                return -1;
-                break;
-            default:
-                // Le père execute ce code
-                // Si & a été écrit, le shell s'affiche directement
-                if (cmd->bg) {
-                    add_jobs(pid1, cpyLine);
-                } else{
-                    waitpid(pid1, &status1, 0);
-                }
-                break;
-        }
-        free(cpyLine);
-        return 0;
+    if (cmd->seq[1]==0) {
+        return exec_simple_cmd(cmd,&cpyLine);
     } 
-// Cas avec pipe    
-    else{
-        if(pipe(pipefd)){
-            perror("error in the pipe creation");
-            return -1;
-        }
-        // Creation du FIlS1 pour faire la partie droite du pipe
-        switch(pid1=fork()) {
-            case -1:
-                //Case d'erreur du fork
-                return -1;
-                break;
-            case 0:
-                // Le fils execute ce code
-                //On dit au fils qu'il va devoir lire son entrée par le pipe qui le lie au pere et plus sur le STDIN
-                dup2(pipefd[0],0);
-                //On ferme le descriteur de fichier en ecriture et en lecture 
-                if (close(pipefd[0])) return -1;
-                if (close(pipefd[1])) return -1;
-                // Et on exec la partie gauche du pipe
-                if ((execvp(cmd->seq[1][0],cmd->seq[1])) == -1){
-                    // Cas d'erreur de l'exec: retourne -1
-                    perror("error in the exec of the children 1");
-                    return -1;
-                }
-                break;
-            default:
-                // Le père execute ce code
-                // Creation du second processus pour la partie gauche du pipe
-                if((pid2=fork()) == 0){
-                    //Le Fils 2 exec ce code
-                    // On dit au Fils2 qu'il ne va plus ecrit sur le STDOUT mais dans le pipe qui le lie au fils
-                    dup2(pipefd[1],1);
-                    //On ferme les acces au pipe 
-                    if (close(pipefd[1])) return -1;
-                    if (close(pipefd[0])) return -1;
-                    //Et on execute la partie droite du pipe
-                    if ((execvp(cmd->seq[0][0],cmd->seq[0])) == -1){
-                        // Cas d'erreur de l'exec: retourne -1
-                        perror("error in the exec of the children 2");
-                        return -1;
-                    }
-                } else if(pid2 == -1){
-                        perror("error in the fork of the children 2");
-                        return -1;
-                }
-                else{
-                    //Le Pere exec ceci 
-                }
-                pid_wait = waitpid(pid2,&status2,0);
-                printf("pere: %d ---  pid1 = %d --- pid2 = %d \n",getpid(), pid1, pid2);
-                printf("PID du proc qui se termine = %d avec status = %d \n",pid_wait, status1);
-                pid_wait = waitpid(pid1,&status1,WNOWAIT);
-                printf("PID du proc qui se termine = %d avec status = %d ",pid_wait,status2);
-                break;
-        } 
-        printf("\n");
+// Cas avec pipe
+    else {
         free(cpyLine);
-        return 0;
+        return exec_pipe_cmd(cmd);
     }
 }
+
+
 
 SCM executer_wrapper(SCM x)
 {
